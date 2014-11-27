271c8d1843939db81249124c0cbde8cfda30fa8f
Stefan Stoichev <stefan.stoichev@gmail.com>
2014-11-27 10:15:32 +0000

    * css fix * support Qlik Sense servers

--- main.js
+++ main.js
@@ -43,39 +43,80 @@ function DrawServers() {
   for(var i = 0; i < servers.length; i++) {
       var serverUrl = servers[i].url;
       var serverName = servers[i].name;
+      var serverType = servers[i].type;
 
-      $('#servers').append('<li>' + serverName + '<ul> \
-      <li class="tab">Status \
-      <ul>\
-      <li class="link" title="'+ serverUrl + '/Default.htm'  +'">Tasks</li>\
-      <li class="link" title="'+ serverUrl + '/ServiceStatusOverview.htm'  +'">Services</li>\
-      <li class="link" title="'+ serverUrl + '/QvsStatistics.htm'  +'">QVS Statistics</li>\
-      </ul>\
-      </li>\
-      \
-      <li class="tab">Documents \
-      <ul>\
-      <li class="link" title="'+ serverUrl + '/SourceDocuments.htm'  +'">Source Documents</li>\
-      <li class="link" title="'+ serverUrl + '/UserDocuments.htm'  +'">User Documents</li>\
-      </ul>\
-      </li>\
-      \
-      <li class="tab">Users \
-      <ul>\
-      <li class="link" title="'+ serverUrl + '/usermanagement.htm'  +'">User Management</li>\
-      <li class="link" title="'+ serverUrl + '/AuthTables.htm'  +'">Section Access Management</li>\
-      </ul>\
-      </li>\
-      \
-      <li class="tab">System \
-      <ul>\
-      <li class="link" title="'+ serverUrl + '/SystemSetup.htm'  +'">Setup</li>\
-      <li class="link" title="'+ serverUrl + '/Licenses.htm'  +'">Licenses</li>\
-      <li class="link" title="'+ serverUrl + '/About.htm'  +'">About</li>\
-      <li class="link" title="'+ serverUrl + '/SupportingTasks.htm'  +'">Supporting Tasks</li>\
-      </ul>\
-      </li>\
-      </ul></li>');
+      if(serverType === 'QlikView') {
+        $('#servers').append('<li>' + serverName + '<ul> \
+        <li class="tab">Status \
+        <ul>\
+        <li class="link" title="'+ serverUrl + '/Default.htm'  +'">Tasks</li>\
+        <li class="link" title="'+ serverUrl + '/ServiceStatusOverview.htm'  +'">Services</li>\
+        <li class="link" title="'+ serverUrl + '/QvsStatistics.htm'  +'">QVS Statistics</li>\
+        </ul>\
+        </li>\
+        \
+        <li class="tab">Documents \
+        <ul>\
+        <li class="link" title="'+ serverUrl + '/SourceDocuments.htm'  +'">Source Documents</li>\
+        <li class="link" title="'+ serverUrl + '/UserDocuments.htm'  +'">User Documents</li>\
+        </ul>\
+        </li>\
+        \
+        <li class="tab">Users \
+        <ul>\
+        <li class="link" title="'+ serverUrl + '/usermanagement.htm'  +'">User Management</li>\
+        <li class="link" title="'+ serverUrl + '/AuthTables.htm'  +'">Section Access Management</li>\
+        </ul>\
+        </li>\
+        \
+        <li class="tab">System \
+        <ul>\
+        <li class="link" title="'+ serverUrl + '/SystemSetup.htm'  +'">Setup</li>\
+        <li class="link" title="'+ serverUrl + '/Licenses.htm'  +'">Licenses</li>\
+        <li class="link" title="'+ serverUrl + '/About.htm'  +'">About</li>\
+        <li class="link" title="'+ serverUrl + '/SupportingTasks.htm'  +'">Supporting Tasks</li>\
+        </ul>\
+        </li>\
+        </ul></li>');
+      } else {
+        $('#servers').append('<li>' + serverName + '<ul> \
+        <li class="tab">Manage Content\
+        <ul>\
+        <li class="link" title="'+ serverUrl + '/apps'  +'">Apps</li>\
+        <li class="link" title="'+ serverUrl + '/contentlibraries'  +'">Content libraries</li>\
+        <li class="link" title="'+ serverUrl + '/dataconnections'  +'">Data connections</li>\
+        <li class="link" title="'+ serverUrl + '/appobjects'  +'">App objects</li>\
+        <li class="link" title="'+ serverUrl + '/streams'  +'">Streams</li>\
+        <li class="link" title="'+ serverUrl + '/tasks'  +'">Tasks</li>\
+        <li class="link" title="'+ serverUrl + '/users'  +'">Users</li>\
+        </ul>\
+        </li>\
+        \
+        <li class="tab">Manage Resources \
+        <ul>\
+        <li class="link" title="'+ serverUrl + '/audit'  +'">Audit</li>\
+        <li class="link" title="'+ serverUrl + '/securityrules'  +'">Security rules</li>\
+        <li class="link" title="'+ serverUrl + '/customproperties'  +'">Custom properties</li>\
+        <li class="link" title="'+ serverUrl + '/licenseandtokens'  +'">License and tokens</li>\
+        <li class="link" title="'+ serverUrl + '/extensions'  +'">Extensions</li>\
+        <li class="link" title="'+ serverUrl + '/tags'  +'">Tags</li>\
+        <li class="link" title="'+ serverUrl + '/userdirectoryconnectors'  +'">User directory connectors</li>\
+        </ul>\
+        </li>\
+        \
+        <li class="tab">Config System \
+        <ul>\
+        <li class="link" title="'+ serverUrl + '/nodes'  +'">Nodes</li>\
+        <li class="link" title="'+ serverUrl + '/engines'  +'">Engines</li>\
+        <li class="link" title="'+ serverUrl + '/proxies'  +'">Proxies</li>\
+        <li class="link" title="'+ serverUrl + '/schedulers'  +'">Schedulers</li>\
+        <li class="link" title="'+ serverUrl + '/repositories'  +'">Repositories</li>\
+        <li class="link" title="'+ serverUrl + '/syncrules'  +'">Sync rules</li>\
+        <li class="link" title="'+ serverUrl + '/certificates'  +'">Certificates</li>\
+        </ul>\
+        </li>\
+        </ul></li>');
+      }
     }
 
     CollapsibleLists.apply();
