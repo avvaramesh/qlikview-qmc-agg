0005a22d9b37a6cfb4ed7a5789ac7d6c8e50ada0
Stefan Stoichev <stefan.stoichev@gmail.com>
2014-11-27 11:40:16 +0000

    Active server view is now displaying on top of the webview

--- main.js
+++ main.js
@@ -49,32 +49,32 @@ function DrawServers() {
         $('#servers').append('<li>' + serverName + '<ul> \
         <li class="tab">Status \
         <ul>\
-        <li class="link" title="'+ serverUrl + '/Default.htm'  +'">Tasks</li>\
-        <li class="link" title="'+ serverUrl + '/ServiceStatusOverview.htm'  +'">Services</li>\
-        <li class="link" title="'+ serverUrl + '/QvsStatistics.htm'  +'">QVS Statistics</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/Default.htm'  +'">Tasks</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/ServiceStatusOverview.htm'  +'">Services</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/QvsStatistics.htm'  +'">QVS Statistics</li>\
         </ul>\
         </li>\
         \
         <li class="tab">Documents \
         <ul>\
-        <li class="link" title="'+ serverUrl + '/SourceDocuments.htm'  +'">Source Documents</li>\
-        <li class="link" title="'+ serverUrl + '/UserDocuments.htm'  +'">User Documents</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/SourceDocuments.htm'  +'">Source Documents</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/UserDocuments.htm'  +'">User Documents</li>\
         </ul>\
         </li>\
         \
         <li class="tab">Users \
         <ul>\
-        <li class="link" title="'+ serverUrl + '/usermanagement.htm'  +'">User Management</li>\
-        <li class="link" title="'+ serverUrl + '/AuthTables.htm'  +'">Section Access Management</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/usermanagement.htm'  +'">User Management</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/AuthTables.htm'  +'">Section Access Management</li>\
         </ul>\
         </li>\
         \
         <li class="tab">System \
         <ul>\
-        <li class="link" title="'+ serverUrl + '/SystemSetup.htm'  +'">Setup</li>\
-        <li class="link" title="'+ serverUrl + '/Licenses.htm'  +'">Licenses</li>\
-        <li class="link" title="'+ serverUrl + '/About.htm'  +'">About</li>\
-        <li class="link" title="'+ serverUrl + '/SupportingTasks.htm'  +'">Supporting Tasks</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/SystemSetup.htm'  +'">Setup</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/Licenses.htm'  +'">Licenses</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/About.htm'  +'">About</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/SupportingTasks.htm'  +'">Supporting Tasks</li>\
         </ul>\
         </li>\
         </ul></li>');
@@ -82,37 +82,37 @@ function DrawServers() {
         $('#servers').append('<li>' + serverName + '<ul> \
         <li class="tab">Manage Content\
         <ul>\
-        <li class="link" title="'+ serverUrl + '/apps'  +'">Apps</li>\
-        <li class="link" title="'+ serverUrl + '/contentlibraries'  +'">Content libraries</li>\
-        <li class="link" title="'+ serverUrl + '/dataconnections'  +'">Data connections</li>\
-        <li class="link" title="'+ serverUrl + '/appobjects'  +'">App objects</li>\
-        <li class="link" title="'+ serverUrl + '/streams'  +'">Streams</li>\
-        <li class="link" title="'+ serverUrl + '/tasks'  +'">Tasks</li>\
-        <li class="link" title="'+ serverUrl + '/users'  +'">Users</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/apps'  +'">Apps</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/contentlibraries'  +'">Content libraries</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/dataconnections'  +'">Data connections</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/appobjects'  +'">App objects</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/streams'  +'">Streams</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/tasks'  +'">Tasks</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/users'  +'">Users</li>\
         </ul>\
         </li>\
         \
         <li class="tab">Manage Resources \
         <ul>\
-        <li class="link" title="'+ serverUrl + '/audit'  +'">Audit</li>\
-        <li class="link" title="'+ serverUrl + '/securityrules'  +'">Security rules</li>\
-        <li class="link" title="'+ serverUrl + '/customproperties'  +'">Custom properties</li>\
-        <li class="link" title="'+ serverUrl + '/tokens'  +'">License and tokens</li>\
-        <li class="link" title="'+ serverUrl + '/extensions'  +'">Extensions</li>\
-        <li class="link" title="'+ serverUrl + '/tags'  +'">Tags</li>\
-        <li class="link" title="'+ serverUrl + '/userdirectoryconnectors'  +'">User directory connectors</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/audit'  +'">Audit</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/securityrules'  +'">Security rules</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/customproperties'  +'">Custom properties</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/tokens'  +'">License and tokens</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/extensions'  +'">Extensions</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/tags'  +'">Tags</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/userdirectoryconnectors'  +'">User directory connectors</li>\
         </ul>\
         </li>\
         \
         <li class="tab">Config System \
         <ul>\
-        <li class="link" title="'+ serverUrl + '/nodes'  +'">Nodes</li>\
-        <li class="link" title="'+ serverUrl + '/engines'  +'">Engines</li>\
-        <li class="link" title="'+ serverUrl + '/proxies'  +'">Proxies</li>\
-        <li class="link" title="'+ serverUrl + '/schedulers'  +'">Schedulers</li>\
-        <li class="link" title="'+ serverUrl + '/repositories'  +'">Repositories</li>\
-        <li class="link" title="'+ serverUrl + '/syncrules'  +'">Sync rules</li>\
-        <li class="link" title="'+ serverUrl + '/certificates'  +'">Certificates</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/nodes'  +'">Nodes</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/engines'  +'">Engines</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/proxies'  +'">Proxies</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/schedulers'  +'">Schedulers</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/repositories'  +'">Repositories</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/syncrules'  +'">Sync rules</li>\
+        <li class="link" data-ref="'+ serverName  +'" title="'+ serverUrl + '/certificates'  +'">Certificates</li>\
         </ul>\
         </li>\
         </ul></li>');
@@ -125,6 +125,7 @@ function DrawServers() {
       var selected = $(this);
       var webview = document.getElementById("webview");
       webview.setAttribute('src', $.trim(selected[0].title));
+      $('#selectedServer').text(jQuery(selected[0]).attr('data-ref'));
     });
 
     $('.tab').on('click', function (){
